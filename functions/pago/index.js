3982e4be50a748fed6fbbf4db599a0aad03048ef
AbaloriaBendita <hola@abaloriabendita.es>
2026-03-12 09:54:39 +0100

    Update index.js

--- functions/pago/index.js
+++ functions/pago/index.js
@@ -85,7 +85,7 @@ export async function onRequestPost(context) {
        URL DE PAGO
     ========================= */
 
-const paymentUrl = `https://pay.sumup.com/b2c/${data.id}`;
+const paymentUrl = `https://checkout.sumup.com/${data.id}`;
     
     return new Response(JSON.stringify({
       payment_url: paymentUrl
