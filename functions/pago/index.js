069f1bd2510180e3dc394908b3887fbc42d86845
AbaloriaBendita <hola@abaloriabendita.es>
2026-03-19 12:07:32 +0100

    Update index.js

--- functions/pago/index.js
+++ functions/pago/index.js
@@ -46,7 +46,7 @@ export async function onRequestPost(context) {
        SQUARE PAYMENT LINK
     ========================= */
 
-    const res = await fetch("https://connect.squareupsandbox.com/v2/online-checkout/payment-links", {
+    const res = await fetch("https://connect.squareup.com/v2/online-checkout/payment-links", {
       method: "POST",
       headers: {
         "Authorization": `Bearer ${env.SQUARE_ACCESS_TOKEN}`,
