65ae38f8423df3cb6199f3b2b3cac2a4a1e1d35f
AbaloriaBendita <hola@abaloriabendita.es>
2026-03-23 10:18:25 +0100

    Delete functions directory

--- functions/pago.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost(context) {
-  console.log("🔥 FUNCTION /pago HIT");
-
-  return new Response(JSON.stringify({
-    ok: true
-  }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
